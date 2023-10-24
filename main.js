b61f00df2676af066f27156392718a64f8d34dcb
iota07 <hanesananhouvannasak@googlemail.com>
2023-10-24 13:06:42 +0200

    200% bonus button update

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ document
   .addEventListener("click", function () {
     alert("Hello! I am an alert box!");
   });
+
+
+
+
+  if (scoreMoney >= 1000 && scoreMoney % 1000 === 0){
+    /*button Appear*/
+    
+  } else {
+    /*button Hidden*/
+  }
+  
+  document.getElementById('boutonpop').addEventListener('click', function(){
+    /*Multiply score X2*/
+  })
