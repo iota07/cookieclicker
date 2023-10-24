43e1892ec42d965e029ae2fe767bb1461518729d
Hanesana Nhouvannasak <145263906+iota07@users.noreply.github.com>
2023-10-24 18:08:28 +0200

    Update main.js
    update Han's branch main.js

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
-document
-  .getElementById("boutonautoclick")
-  .addEventListener("click", function () {
-    alert("Hello! I am an alert box!");
-  });
+function checkMoney(scoreMoney) {
 
-
-
-
-  if (scoreMoney >= 1000 && scoreMoney % 1000 === 0){
-    /*button Appear*/
-    
+  if (scoreMoney < 1000) {
+      document.getElementById('boutonpop').style.display = 'none'; /*if money <1000 button hidden*/
+  } else if (scoreMoney >= 1000 && scoreMoney % 1000 === 0) {
+      document.getElementById('boutonpop').style.display = 'block'; /*if money > 1000 && every 1000 multiplier button Appear for 30sec*/
+      setTimeout(function () {
+          document.getElementById('boutonpop').style.display = 'none';
+      }, 30000);
   } else {
-    /*button Hidden*/
+      document.getElementById('boutonpop').style.display = 'none'; /*else hidden*/
   }
-  
-  document.getElementById('boutonpop').addEventListener('click', function(){
-    /*Multiply score X2*/
-  })
+}
+
+
+
+document.getElementById('boutonpop').addEventListener('click', function(){
+  scoreGlobal = scoreValue*2 /*Multiply score click X2*/
