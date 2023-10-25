f8dcaa1c7ccff47b81af2084eec7beef3b5c7f77
Julia Michel T. <juliamicheltex@gmail.com>
2023-10-25 17:17:18 +0200

    Bootstrap + popper + sass

---
+++ src/main.js
@@ -0,0 +1,73 @@
+// Import our custom CSS
+import "./scss/styles.scss";
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
+document.addEventListener("DOMContentLoaded", function () {
+  // NOS DECLARATIONS
+
+  let i = 1;
+  let points = 0;
+  let autoClickEnabled = false;
+  let autoClickInterval;
+
+  const pointsElement = document.getElementById("scoreclickbonus");
+  const startButton = document.getElementById("boutonautoclick");
+  const cookieclic = document.getElementById("cookie1");
+  const pointsnobonus = document.getElementById("scoreclick");
+  const pointsmoney = document.getElementById("scoremoney");
+
+  //FONCTION COOKIECLIC
+
+  cookieclic.addEventListener("click", () => {
+    points += i;
+    pointsnobonus.textContent = points;
+    let moneyValue = parseInt(pointsmoney.textContent);
+    moneyValue++;
+    pointsmoney.textContent = moneyValue;
+  });
+
+  //FONCTION AUTOCLICK
+
+  startButton.addEventListener("click", () => {
+    if (!autoClickEnabled && points >= 50) {
+      subtractPoints(50);
+      startAutoClick();
+      autoClickEnabled = true;
+      startButton.style.display = "none";
+    }
+  });
+
+  function autoClick() {
+    addPoints(+1);
+  }
+
+  function startAutoClick() {
+    // TEMPS DU CLICK
+    autoClickInterval = setInterval(autoClick, 500);
+  }
+
+  /**
+   * @description rajouter des points dans html
+   * @param {number} value
+   */
+
+  function addPoints() {
+    let moneyValue = parseInt(pointsmoney.textContent);
+    moneyValue++;
+    pointsmoney.textContent = moneyValue;
+  }
+
+  function subtractPoints(value) {
+    pointsmoney.textContent -= value;
+  }
+
+  // Appelez cette fonction au chargement de la page pour initialiser le score.
+  function initializeGame() {
+    points = 0;
+    pointsElement.textContent = 0;
+    pointsmoney.textContent = 0;
+  }
+
+  initializeGame();
+});
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,27 @@
+import path from "path";
+import { resolve } from "path";
 import { defineConfig } from "vite";
+
 export default defineConfig({
   base: "/cookieclicker/",
+  root: path.resolve(__dirname, "src"),
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
+  },
+  server: {
+    port: 8080,
+    hot: true,
+  },
+  build: {
+    outDir: path.resolve(__dirname, "dist"), // Le répertoire de sortie pour les fichiers construits
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "./src/main.js"),
+        index: path.resolve(__dirname, "./src/index.html"),
+      },
+    },
+    emptyOutDir: true,
+  },
 });
