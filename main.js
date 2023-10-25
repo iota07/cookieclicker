1c1aa51105e79a843ea38fca75cdf3760a2f2df3
Ezaabouji Zineb <Ezaabouji.zineb@outlook.com>
2023-10-25 13:52:47 +0200

    ajout js

--- main.js
+++ main.js
@@ -1,53 +1,43 @@
-// Récupérez les éléments du bouton Multi, le score de clic et l'image du cookie
 const boutonMulti = document.getElementById("boutonmulti");
-const scoreClickbonus = document.getElementById("scoreclickbonus");
-const imageCookie = document.querySelector(".cookie img");
-const scoreClick = document.getElementById("scoreclick");
+        const scoreClickbonus = document.getElementById("scoreclickbonus");
+        const imageCookie = document.querySelector(".cookie img");
+        const scoreClick = document.getElementById("scoreclick");
 
-// Définissez le coût initial du bonus et l'état du bonus
-let bonusCost = 7;
-let bonusMultiplier = 1;
-let score = 0;
-let scoreBonus = 0;
+        let bonusCost = 7;
+        let bonusMultiplier = 1;
+        let score = 0;
+        let scoreBonus = 0;
 
-document.getElementById('cookieClick').addEventListener('click', function() {
+        document.getElementById('cookieClick').addEventListener('click', function() {
+            score += 1;
+            scoreBonus += bonusMultiplier; // Mise à jour du score bonus
 
-    score += 1;
-    scoreBonus += 1;
+            document.getElementById('scoreclick').innerText = score;
+            document.getElementById('scoreclickbonus').innerText = scoreBonus;
+            updateMultiButton();
+        });
 
-    document.getElementById('scoreclick').innerText = score;
-    document.getElementById('scoreclickbonus').innerText = scoreBonus;
-    updateMultiButton();
-});
+        function updateMultiButton() {
+            if (score >= bonusCost) {
+                boutonMulti.style.display = "block";
+            } else if (score >= 20) {
+                boutonMulti.style.display = "block";
+            } else {
+                boutonMulti.style.display = "none";
+            }
+        }
 
+        boutonMulti.addEventListener("click", function () {
+            if (parseInt(scoreClick.textContent) >= bonusCost) {
+                scoreClick.textContent = parseInt(scoreClick.textContent) - bonusCost; // Déduction du score
+                bonusMultiplier *= 2; // Doublement du multiplicateur
+                bonusCost *= 2; // Doublement du coût du bonus
 
-// Fonction pour mettre à jour le bouton Multi en fonction du score
-function updateMultiButton() {
-    if (score >= 10) {
-        boutonMulti.style.display = "block";
-    } else {
-        boutonMulti.style.display = "none";
-    }
-}
+                //boutonMulti.textContent = `Acheter le bonus Multi (${bonusCost})`;
+                scoreClickbonus.textContent = bonusMultiplier - 1;
 
-// Gérez l'achat du bonus Multi
-boutonMulti.addEventListener("click", function () {
-    if (parseInt(scoreClick.textContent) >= bonusCost) {
-        scoreClickbonus.textContent = parseInt(scoreClick.textContent) - bonusCost; // Déduction du score
-        bonusMultiplier *= 5; // Augmentation du multiplicateur
-        bonusCost += 7; // Augmentation du coût du bonus
-
-        // Mettez à jour le texte du bouton
-        boutonMulti.textContent = `Multiplier (Coût : ${bonusCost})`;
-
-        // Mettez à jour le texte du score bonus
-        scoreClickbonus.textContent = `Score Click Bonus: ${bonusMultiplier - 1}`;
+                updateMultiButton();
+            }
+        });
 
         updateMultiButton();
-    }
-});
-
-
-
-// Mettez à jour le bouton Multi au chargement de la page
-updateMultiButton();
